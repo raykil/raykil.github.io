03d0226d69b99dfabe6ef245a31ac9a6686c532b
Raymond Kil <jkil@nd.edu>
2025-11-02 01:47:26 -0400

    trading algo separated and assembled in trade func

--- fpga-hft/trade.py
+++ fpga-hft/trade.py
@@ -14,21 +14,28 @@ async def subscribe(websocket, tickers: list[str]): # Ex) tickers: ["AAPL", "TSL
     await websocket.send(json.dumps(subscribe_msg))
     print("✅ Subscription request successfully made!")
 
-async def receiveQuotes(c, tickers: list[str]):
+async def receiveQuotes(websocket):
+    async for msg in websocket:
+        print(f"Original message was: {msg}")
+        for quote in json.loads(msg):
+            if quote['T']=='q': yield quote
+
+async def trade(c, tickers: list[str]):
     async with websockets.connect(c['stream_url']) as websocket:
         await authenticate(websocket, c)
         await subscribe(websocket, tickers)
-        async for msg in websocket:
-            data = json.loads(msg)
-            print(data)
+        quote_iterator = receiveQuotes(websocket)
+        async for quote in quote_iterator:
+            print(quote, type(quote))
+
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog='websocket.py', epilog="jkil@nd.edu")
-    parser.add_argument('-m', '--mode', default="paper", type=str, help="'paper' for paper trading sandbox, 'live' for live trading.")
+    parser.add_argument('-m', '--mode', default="paper", type=str, help="Keys in config.json.")
     parser.add_argument('-s', '--strategy', default="momentum", type=str, help="Options: momentum only for now")
     parser.add_argument('-t', '--tickers', default="BTC/USD", type=str, help="Options: momentum only for now")
     args = parser.parse_args()
 
     scriptPath = os.path.dirname(os.path.abspath(__file__))
     config = loadConfig(f"{scriptPath}/config.json", args.mode)
-    asyncio.run(receiveQuotes(config, ["BTC/USD"]))
+    asyncio.run(trade(config, ["BTC/USD"]))
