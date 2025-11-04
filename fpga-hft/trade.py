88aac64ae01c18f04c63a0cbc02ac1655bc5da0c
Raymond Kil <jkil@nd.edu>
2025-11-04 18:47:31 -0500

    stable pretty plotting

--- fpga-hft/trade.py
+++ fpga-hft/trade.py
@@ -30,26 +30,54 @@ async def receiveMessages(websocket):
             elif msg['T']=='subscription': print("✅ Subscribed to:", {k: v for k, v in msg.items() if isinstance(v, list) and v})
             elif (msg['T']=='b')|(msg['T']=='u'):
                 timestamp = datetime.fromisoformat(msg['t']).astimezone(ZoneInfo("America/New_York")).replace(tzinfo=None)
-                message = {
-                    'Open': msg['o'], 'High': msg['h'], 'Low': msg['l'], 'Close': msg['c'], 'Volume': msg['v'], 
-                    'avgPrice': msg['vw']
-                }
+                message = {'Open': msg['o'], 'High': msg['h'], 'Low': msg['l'], 'Close': msg['c'], 'Volume': msg['v'], 'avgPrice': msg['vw']}
                 yield timestamp, message
 
-def plotBars(BARS, axes):
+def plotBars(BARS, axes, symbols):
+    textcolor = 'whitesmoke'
+    candle_colors = mpf.make_marketcolors(up='limegreen', down='red', wick='silver', edge='silver', volume='blue')
+    candle_style = mpf.make_mpf_style(marketcolors=candle_colors)
+    plotConfig = {
+        'avgLine'   : {'width': 1, 'color': 'royalblue', 'label':'Volume-weighted average price'},
+        'avgScatter': {'type': 'scatter', 'color': 'royalblue'},
+        'candle'    : {'type': 'candle' , 'returnfig': True, 'figsize':(14,8), 'style': candle_style},
+        'title'     : {'color': textcolor, 'fontsize': 16, 'fontweight': 'bold'},
+        'labels'    : {'color': textcolor, 'fontsize': 14},
+        'tickmarks' : {'colors': textcolor}
+    }
     if axes is None:
-        avgPlot = mpf.make_addplot(BARS['avgPrice'], width=1, color='royalblue')
-        axes = mpf.plot(BARS[['Open', 'High', 'Low', 'Close', 'Volume']], type='candle', addplot=avgPlot, returnfig=True)[1]
-        return axes
-    axes[0].clear()
-    avgPlot = mpf.make_addplot(BARS['avgPrice'], width=1, color='royalblue', ax=axes[0])
-    mpf.plot(BARS[['Open', 'High', 'Low', 'Close', 'Volume']], type='candle', addplot=avgPlot, ax=axes[0])
+        fig, axes = mpf.plot(BARS[['Open', 'High', 'Low', 'Close', 'Volume']], **plotConfig['candle'])
+    else:
+        axes[0].clear()
+        for p in ['avgLine', 'avgScatter', 'candle']: plotConfig[p]['ax'] = axes[0]
+        additional_plots = [
+            mpf.make_addplot(BARS['avgPrice'], **plotConfig['avgLine']),
+            mpf.make_addplot(BARS['avgPrice'], **plotConfig['avgScatter'])
+        ]
+        plotConfig['candle'].pop('figsize')
+        plotConfig['candle'].pop('returnfig')
+        plotConfig['candle']['addplot'] = additional_plots
+        mpf.plot(BARS[['Open', 'High', 'Low', 'Close', 'Volume']], **plotConfig['candle'])
+        fig = axes[0].figure
+
+    # Style
+    pos = axes[0].get_position()
+    L, R = 0.08, 0.93
+    fig.patch.set_facecolor('#1c2129')
+    axes[0].set_facecolor('#22272d')
+    axes[0].set_xlabel("Time(hh:mm) EST", **plotConfig['labels'])
+    axes[0].set_ylabel("Price (USD)", **plotConfig['labels'])
+    axes[0].tick_params(axis='x', **plotConfig['tickmarks'])
+    axes[0].tick_params(axis='y', **plotConfig['tickmarks'])
+    axes[0].grid(which='both', color="#39424c")
+    axes[0].set_title(''.join(symbols), **plotConfig['title'])
+    axes[0].set_position([L, pos.y0, R-L, pos.height])
+    axes[0].margins(x=0.001)
     plt.pause(0.001)
     return axes
 
 def appendBars(BARS, timestamp, msg):
     BARS.loc[timestamp] = msg
-    BARS = BARS.iloc[-29:]
     print(timestamp, BARS.iloc[-1].to_dict())
 
 async def trade(c, symbols: list[str]):
@@ -60,7 +88,7 @@ async def trade(c, symbols: list[str]):
         plt.ion(); axes=None
         async for timestamp, msg in receiveMessages(websocket):
             appendBars(BARS, timestamp, msg)
-            axes = plotBars(BARS, axes)
+            axes = plotBars(BARS, axes, symbols)
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog='websocket.py', epilog="jkil@nd.edu")
